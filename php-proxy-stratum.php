4db08c3bfe689c87d3504cd9c3d17e67e0e20a6b
Carles Tubio <ctubio@users.noreply.github.com>
2015-11-14 02:59:37 +0100

    Fixed submitting stale share error in miners.

--- php-proxy-stratum.php
+++ php-proxy-stratum.php
@@ -2,11 +2,11 @@
 class Stratum {
   private $s = array();
   private $p = array();
-  private $m = array();
+  private $o = array();
 
   public function __construct() {
     set_time_limit(0);
-    $this->p = $this->m = array(NULL);
+    $this->p = $this->o = array(NULL);
     $this->s = array(socket_create(AF_INET, SOCK_STREAM, SOL_TCP));
     socket_set_option($this->s[0], SOL_SOCKET, SO_REUSEADDR, 1);
     socket_bind($this->s[0], 0, 3333) or die('ERROR: Could not bind to address.');
@@ -23,7 +23,7 @@ class Stratum {
   private function x($r) {
     if (in_array($this->s[0], $r)) {
       $this->p[] = NULL;
-      $this->m[] = array();
+      $this->o[] = array();
       $this->s[] = socket_accept($this->s[0]);
       $this->l('connected, total: '.(count($this->s) - 1).'.');
       unset($r[array_search($this->s[0], $r)]);
@@ -34,21 +34,20 @@ class Stratum {
       if ($_d === FALSE) $this->k($k, 'lost');
       else if (!$_k) {
         if ($this->s[$k]) {
-          $this->l($k.' gets: '.$_d);
-          socket_write($this->s[$k], $_d);
-          if (isset($this->m[$k][1]) && strpos($_d,'mining.notify')!==FALSE) {
-            socket_write($this->p[$k], $this->m[$k][1]);
-            $this->l('server '.$k.' gets '.$this->m[$k][1]);
-            unset($this->m[$k][1]);
+          if (isset($this->o[$k][1]) && strpos($_d,'mining.notify')!==FALSE) {
+            socket_write($this->p[$k], $this->o[$k][1]);
+            $this->l('server '.$k.' gets '.$this->o[$k][1]);
+            unset($this->o[$k][1]);
           }
-          if (strpos($_d,'mining.notify')!==FALSE && strpos($_d,'mining.set_difficulty')!==FALSE) {
+          if (strpos($_d,'mining.set_difficulty')!==FALSE && strpos($_d,'mining.notify')!==FALSE) {
             $d = json_decode($_d);
-            if (is_object($d) && isset($d->result) && isset($d->result[1]) && $d->result[1])
-              socket_write($this->s[$k], json_encode(array(
-                'params'=>array($d->result[1],$d->result[2]),
-                'method'=>'mining.set_extranonce',
-                'id'=>NULL
-              ))."\n");
+            if (is_object($d) && isset($d->result) && isset($d->result[1]) && $d->result[1]) {
+              $this->l($k.' gets extranonce ["'.$d->result[1].'", '.$d->result[2].'].');
+              socket_write($this->s[$k], '{"params":["'.$d->result[1].'",'.$d->result[2].'],"method":"mining.set_extranonce","id":null}'."\n");
+            }
+          } else {
+            $this->l($k.' gets: '.$_d);
+            socket_write($this->s[$k], $_d);
           }
         } else $this->k($k, 'lost before server');
       } else {
@@ -60,19 +59,21 @@ class Stratum {
               socket_write($this->p[$k], $_d);
             } else {
               socket_write($this->s[$k], '{"id":'.$d->id.',"result":[[["mining.set_difficulty","1"],["mining.notify","1"]],"00",4],"error":null}'."\n");
-              $this->m[$k][0] = $_d;
-              $this->l($k.' gets subscription.');
+              $this->o[$k][0] = $_d;
+              $this->l($k.' gets subscription '.$d->id.'.');
             }
           } else if ($d->method == 'mining.authorize' and !$this->p[$k]) {
             $this->p[$k] = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
             if (!socket_connect($this->p[$k], 'eu.stratum.bitcoin.cz', 3333)
               && !socket_connect($this->p[$k], 'stratum.f2pool.com', 3333))
               $this->k($k, 'lost pools');
-            else if (isset($this->m[$k][0])) {
-              $this->l('server '.$k.' gets '.$this->m[$k][0]);
-              socket_write($this->p[$k], $this->m[$k][0]);
-              unset($this->m[$k][0]);
-              $this->m[$k][1] = $_d;
+            else if (isset($this->o[$k][0])) {
+              $this->l('server '.$k.' gets '.$this->o[$k][0]);
+              socket_write($this->p[$k], $this->o[$k][0]);
+              $this->l($k.' gets authorization '.$d->id.'.');
+              socket_write($this->s[$k], '{"error":null,"id":'.$d->id.',"result":true}'."\n");
+              unset($this->o[$k][0]);
+              $this->o[$k][1] = $_d;
             }
           } else if ($this->p[$k]) {
             $this->l('server '.$k.' gets '.$_d);
@@ -84,10 +85,10 @@ class Stratum {
   }
 
   private function k($k, $m) {
-    unset($this->s[$k], $this->p[$k], $this->m[$k]);
+    unset($this->s[$k], $this->p[$k], $this->o[$k]);
     $this->s = array_values($this->s);
     $this->p = array_values($this->p);
-    $this->m = array_values($this->m);
+    $this->o = array_values($this->o);
     $this->l($k.' '.$m.', killed.');
   }
 
