202fba2d6cfec07d8525e7389964fcbb11f8eac8
Carles Tubio <ctubio@users.noreply.github.com>
2015-11-15 18:04:51 +0100

    Commented out example lines.

--- pub/php-proxy-stratum.php
+++ pub/php-proxy-stratum.php
@@ -6,8 +6,8 @@ socket_set_option($x, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 5, 'usec' => 50000
 socket_connect($x, 0, 8033);
 socket_write($x, '{"method":"wtfisconnected"}'."\n");
 $wtf = trim(@socket_read($x, 2048, PHP_NORMAL_READ));
-socket_write($x, '{"method":"switchpool","params":["analpaper.3", 0]}'."\n");
-var_dump(@socket_read($x, 2048, PHP_NORMAL_READ));
+#socket_write($x, '{"method":"switchpool","params":["analpaper.3", 0]}'."\n");
+#var_dump(@socket_read($x, 2048, PHP_NORMAL_READ));
 socket_close($x);
 print '<h1>wtfisconnected</h1>';
 print '<pre>';
