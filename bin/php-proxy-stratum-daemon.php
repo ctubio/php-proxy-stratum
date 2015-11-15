1b9f6e6fea8f8375858363b6ebd7840ee65d0eee
Carles Tubio <ctubio@users.noreply.github.com>
2015-11-16 00:55:27 +0100

    Added average hashrate.

--- bin/php-proxy-stratum-daemon.php
+++ bin/php-proxy-stratum-daemon.php
@@ -47,38 +47,42 @@ class Stratum {
     foreach($r as $_r) {
       $k = ($_k = array_search($_r, $this->s)) ?: array_search($_r, $this->p);
       $_d = $this->o[$k]->d(@socket_read($_r, 2048, PHP_NORMAL_READ));
-      if ($_d === FALSE) $this->k($k, 'lost');
+      if ($_d === FALSE || !($d = json_decode($_d, TRUE))) $this->k($k, 'lost');
       else if ($_k === FALSE) {
         if ($this->s[$k]) {
-          $d = json_decode($_d);
-          if (isset($d->id) && $d->id && $d->id == $this->o[$k]->s[0]) {
-            if (isset($d->result) && isset($d->result[1]) && $d->result[1]) {
-              $this->l($k.' gets extranonce ["'.$d->result[1].'", '.$d->result[2].'].');
-              socket_write($this->s[$k], '{"params":["'.$d->result[1].'",'.$d->result[2].'],"method":"mining.set_extranonce","id":null}'."\n");
+          if (isset($d['id']) && $d['id'] && $d['id'] == $this->o[$k]->s[0]) {
+            if (isset($d['result']) && isset($d['result'][1]) && $d['result'][1]) {
+              $this->l($k.' gets extranonce ["'.$d['result'][1].'", '.$d['result'][2].'].');
+              socket_write($this->s[$k], '{"params":["'.$d['result'][1].'",'.$d['result'][2].'],"method":"mining.set_extranonce","id":null}'."\n");
             }
-          } else if(!isset($d->method) || $d->method!='client.show_message') {
+          } else if(!isset($d['method']) || $d['method']!='client.show_message') {
             $this->l($k.' gets: '.$_d);
             socket_write($this->s[$k], $_d);
           }
+          if(isset($d['method']) && $d['method']=='mining.set_difficulty' && isset($d['params']) && isset($d['params'][0]))
+            $this->o[$k]->F = $d['params'][0];
+          $this->t($k);
         } else $this->k($k, 'lost before server');
       } else {
         $this->l($k.' says: '.$_d);
-        if (($d = json_decode($_d)) && isset($d->method)) {
-          if ($d->method == 'mining.subscribe') {
-            $this->l($k.' gets subscription '.$d->id.'.');
-            socket_write($this->s[$k], '{"id":'.$d->id.',"result":[[["mining.set_difficulty","1"],["mining.notify","1"]],"00",4],"error":null}'."\n");
+        if (isset($d['method'])) {
+          if ($d['method'] == 'mining.subscribe') {
+            $this->l($k.' gets subscription '.$d['id'].'.');
+            socket_write($this->s[$k], '{"id":'.$d['id'].',"result":[[["mining.set_difficulty","1"],["mining.notify","1"]],"00",4],"error":null}'."\n");
             if (!$this->p[$k]) {
-              $this->o[$k]->v = (isset($d->params) && isset($d->params[0]) && $d->params[0]) ? $d->params[0] : 'unknown';
-              $this->o[$k]->s = array($d->id, $_d);
+              $this->o[$k]->v = (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) ? $d['params'][0] : 'unknown';
+              $this->o[$k]->s = array($d['id'], $_d);
             }
-          } else if ($d->method == 'mining.authorize') {
-            $this->l($k.' gets authorization '.$d->id.'.');
-            socket_write($this->s[$k], '{"error":null,"id":'.$d->id.',"result":true}'."\n");
-            if (isset($d->params) && isset($d->params[0]) && $d->params[0]) {
-              $this->o[$k]->u = $d->params[0];
+          } else if ($d['method'] == 'mining.authorize') {
+            $this->l($k.' gets authorization '.$d['id'].'.');
+            socket_write($this->s[$k], '{"error":null,"id":'.$d['id'].',"result":true}'."\n");
+            if (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) {
+              $this->o[$k]->u = $d['params'][0];
               $this->c($k);
             } else $this->k($k, 'unkown.');
           } else if ($this->p[$k]) {
+            if(isset($d['method']) && $d['method']=='mining.submit' && isset($d['params']) && isset($d['params'][0]) and $d['params'][0]==$this->o[$k]->P[2])
+              $this->t($k, TRUE);
             $this->l('server '.$k.' gets '.$_d);
             socket_write($this->p[$k], $_d);
           } else $this->k($k, 'lost server');
@@ -106,23 +110,46 @@ class Stratum {
     $this->l($k.' '.$m.', killed.');
   }
 
+  private function t($k, $F = FALSE) {
+    if ($F) {
+      $this->o[$k]->S300 += $this->o[$k]->F;
+      $this->o[$k]->S3600 += $this->o[$k]->F;
+      $this->o[$k]->S86400 += $this->o[$k]->F;
+    }
+    if ($this->o[$k]->St300<time()) {
+      $this->o[$k]->H300 = number_format((((pow(2, 48)/65535)*$this->o[$k]->S300)/300)/exp(21), 2, ',', '.');
+      $this->o[$k]->S300 = 0;
+      $this->o[$k]->St300 = time() + 300;
+    }
+    if ($this->o[$k]->St3600<time()) {
+      $this->o[$k]->H3600 = number_format((((pow(2, 48)/65535)*$this->o[$k]->S3600)/3600)/exp(21), 2, ',', '.');
+      $this->o[$k]->S3600 = 0;
+      $this->o[$k]->St3600 = time() + 3600;
+    }
+    if ($this->o[$k]->St86400<time()) {
+      $this->o[$k]->H86400 = number_format((((pow(2, 48)/65535)*$this->o[$k]->S86400)/86400)/exp(21), 2, ',', '.');
+      $this->o[$k]->S86400 = 0;
+      $this->o[$k]->St86400 = time() + 86400;
+    }
+  }
+
   private function h($h) {
     $this->l('HTTP says '.$h);
     $d = array('result'=>NULL);
-    if (is_object($h = @json_decode($h)) && isset($h->method))
-      switch($h->method) {
+    if (($h = @json_decode($h, TRUE)) && isset($h['method']))
+      switch($h['method']) {
         case 'wtfisconnected':
           foreach($this->o as $k => $o) {
             if (!$o) continue;
             if ($o->u)
-              $d['result'][] = $o->u.' is fuckin connected with '.$o->v.' to '.$o->P[0].' as '.$o->P[2].'.';
+              $d['result'][] = $o->u.' is fuckin connected with '.$o->v.' to '.$o->P[0].' at '.$o->F.' diff and '.$o->H300.'GH/s 5min avg and '.$o->H3600.'GH/s 1hour avg and '.$o->H86400.'GH/s 1day avg hashrate as '.$o->P[2].'.';
             else $d['result'][] = $k.' is zombie.';
           }
           break;
         case 'switchpool':
           foreach($this->o as $k => $o) {
-            if (!$o || $o->u!=$h->params[0]) continue;
-            $this->c($k, $h->params[1]);
+            if (!$o || $o->u!=$h['params'][0]) continue;
+            $this->c($k, $h['params'][1]);
             break;
           }
           break;
@@ -130,7 +157,7 @@ class Stratum {
     return json_encode($d);
   }
 
-  private function l($m) {
+  private function l($m) {# return;
     print date('H:i:s') .': Client '.$m.(strpos($m, "\n")===FALSE ? PHP_EOL : NULL);
   }
 }
@@ -138,8 +165,20 @@ class Stratum {
 class U {
   public $v = NULL;
   public $s = NULL;
+  public $H300 = 0;
+  public $S300 = 0;
+  public $St300 = 0;
+  public $H3600 = 0;
+  public $S3600 = 0;
+  public $St3600 = 0;
+  public $H86400 = 0;
+  public $S86400 = 0;
+  public $St86400 = 0;
+  public $F = 0;
   public $P = NULL;
   private $p = NULL;
+
+  public function __get($k) { return NULL; }
 
   public function __set($k, $v) {
     $this->$k = $v;
@@ -147,6 +186,12 @@ class U {
       $this->P = array('solo.ckpool.org', 3333, '1CArLeSkmBT1BkkcADtNrHoLSgHVhBcesk');
       $this->p = array(
         'analpaper.3' => array(
+          'p' => array(
+            array('eu.stratum.bitcoin.cz', 3333, 'analpaper.0'),
+            array('stratum.f2pool.com', 3333, 'analpaper.0')
+          )
+        ),
+        'analpaper.2' => array(
           'p' => array(
             array('eu.stratum.bitcoin.cz', 3333, 'analpaper.0'),
             array('stratum.f2pool.com', 3333, 'analpaper.0')
