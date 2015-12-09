9a4b95c7a06b1b683b6ca1546d12904ec2352585
Carles Tubio <ctubio@users.noreply.github.com>
2015-12-09 17:58:21 +0000

    Added reactphp.

--- bin/php-proxy-stratum-daemon.php
+++ bin/php-proxy-stratum-daemon.php
@@ -6,103 +6,111 @@ class Stratum {
   private $o = array();
 
   public function __construct() {
-    $this->o = array(NULL);
-    $this->p = array(socket_create(AF_INET, SOCK_STREAM, SOL_TCP));
-    socket_set_option($this->p[0], SOL_SOCKET, SO_REUSEADDR, 1);
-    socket_bind($this->p[0], 0, 8033) || die('!8033');
-    socket_listen($this->p[0]);
-    $this->s = array(socket_create(AF_INET, SOCK_STREAM, SOL_TCP));
-    socket_set_option($this->s[0], SOL_SOCKET, SO_REUSEADDR, 1);
-    socket_bind($this->s[0], 0, 3333) || die('!3333');
-    socket_listen($this->s[0]);
-    set_time_limit(0);
-    for (;;) {
-      $r = array_merge($this->s, array_filter($this->p));
-      if (socket_select($r, $w = NULL, $e = NULL, 0)) $this->x($r);
-    }
-  }
-
-  public function __destruct() {
-    socket_close($this->p[0]);
-    socket_close($this->s[0]);
-  }
-
-  private function x($r) {
-    if (in_array($this->p[0], $r)) {
-      $k = socket_accept($this->p[0]);
-      socket_getpeername($k , $a);
-      if ($a=='127.0.0.1')
-        while ($a = @socket_read($k, 2048, PHP_NORMAL_READ))
-          socket_write($k, $this->h($a)."\n");
-      unset($r[array_search($this->p[0], $r)]);
-    }
-    if (in_array($this->s[0], $r)) {
+    require dirname(__DIR__) . '/vendor/autoload.php';
+    $l = React\EventLoop\Factory::create();
+    $s = new React\Socket\Server($l);
+    $s->on('connection', function ($c) use ($l) {
       if (($k = count($this->s))<9999) {
         $this->p[] = NULL;
         $this->o[] = new U();
-        $this->s[] = socket_accept($this->s[0]);
-        $this->l('connected, total: '.$k.'.');
+        $this->s[] = $c;
+        $this->l('connected, total: '.($k+1).'.');
       } else $this->l('ignored, too many.');
-      unset($r[array_search($this->s[0], $r)]);
-    }
-    foreach($r as $_r) {
-      $k = ($_k = array_search($_r, $this->s)) ?: array_search($_r, $this->p);
-      $_d = $this->o[$k]->d(@socket_read($_r, 2048, PHP_NORMAL_READ));
-      if ($_d === FALSE || !($d = json_decode($_d, TRUE))) $this->k($k, 'lost');
-      else if ($_k === FALSE) {
-        if ($this->s[$k]) {
-          if (isset($d['id']) && $d['id'] && $d['id'] == $this->o[$k]->s[0]) {
-            if (isset($d['result']) && isset($d['result'][1]) && $d['result'][1]) {
-              $this->l($k.' gets extranonce ["'.$d['result'][1].'", '.$d['result'][2].'].');
-              socket_write($this->s[$k], '{"params":["'.$d['result'][1].'",'.$d['result'][2].'],"method":"mining.set_extranonce","id":null}'."\n");
-            }
-          } else if(!isset($d['method']) || $d['method']!='client.show_message') {
-            $this->l($k.' gets: '.$_d);
-            socket_write($this->s[$k], $_d);
+      $c->on('close', function ($c) {
+        $this->k(array_search($c, $this->s), 'gone');
+      });
+      $c->on('data', function ($d, $c) use ($l) {
+        $this->x($d, $c, $l);
+      });
+    });
+    $s->listen(3333, 0);
+    $w = new React\Socket\Server($l);
+    $w->on('connection', function ($c) use ($l) {
+      $c->on('data', function ($d, $c) use ($l) {
+        if ($c->getRemoteAddress()=='127.0.0.1')
+          $this->h($d, $c, $l);
+      });
+    });
+    $w->listen(8033, 0);
+    set_time_limit(0);
+    $l->run();
+  }
+
+  private function x($_d, $_r, $l) {
+    $k = array_search($_r, $this->s);
+    $_d = $this->o[$k]->d($_d);
+    if ($_d === FALSE || !($d = json_decode($_d, TRUE))) $this->k($k, 'lost');
+    else {
+      $this->l(((int)$k).' says: '.$_d);
+      if (isset($d['method'])) {
+        if ($d['method'] == 'mining.subscribe') {
+          $this->l(((int)$k).' gets subscription '.$d['id'].'.');
+          $this->s[$k]->write('{"id":'.$d['id'].',"result":[[["mining.set_difficulty","1"],["mining.notify","1"]],"00",4],"error":null}'."\n");
+          if (!$this->p[$k]) {
+            $this->o[$k]->v = (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) ? $d['params'][0] : 'unknown';
+            $this->o[$k]->s = array($d['id'], $_d);
           }
-          if (isset($d['method']) && $d['method']=='mining.set_difficulty' && isset($d['params']) && isset($d['params'][0]))
-            $this->o[$k]->F = $d['params'][0];
-          if (isset($d['result']) && $d['result']===true && isset($d['id']) && $d['id'])
-          $this->o[$k]->t($d['id']);
-        } else $this->k($k, 'lost before server');
-      } else {
-        $this->l($k.' says: '.$_d);
-        if (isset($d['method'])) {
-          if ($d['method'] == 'mining.subscribe') {
-            $this->l($k.' gets subscription '.$d['id'].'.');
-            socket_write($this->s[$k], '{"id":'.$d['id'].',"result":[[["mining.set_difficulty","1"],["mining.notify","1"]],"00",4],"error":null}'."\n");
-            if (!$this->p[$k]) {
-              $this->o[$k]->v = (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) ? $d['params'][0] : 'unknown';
-              $this->o[$k]->s = array($d['id'], $_d);
-            }
-          } else if ($d['method'] == 'mining.authorize') {
-            $this->l($k.' gets authorization '.$d['id'].'.');
-            socket_write($this->s[$k], '{"error":null,"id":'.$d['id'].',"result":true}'."\n");
-            if (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) {
-              $this->o[$k]->u = $d['params'][0];
-              $this->c($k);
-            } else $this->k($k, 'unkown');
-          } else if ($this->p[$k]) {
-            if(isset($d['method']) && $d['method']=='mining.submit' && isset($d['params']) && isset($d['params'][0]) and $d['params'][0]==$this->o[$k]->P['user'])
-              $this->o[$k]->t(-$d['id']);
-            $this->l('server '.$k.' gets '.$_d);
-            socket_write($this->p[$k], $_d);
-          } else $this->k($k, 'lost server');
-        } else $this->k($k, 'said garbage');
-      }
+        } else if ($d['method'] == 'mining.authorize') {
+          $this->l(((int)$k).' gets authorization '.$d['id'].'.');
+          $this->s[$k]->write('{"error":null,"id":'.$d['id'].',"result":true}'."\n");
+          if (isset($d['params']) && isset($d['params'][0]) && $d['params'][0]) {
+            $this->o[$k]->u = $d['params'][0];
+            $this->c($l, $k);
+          } else $this->k($k, 'unkown');
+        } else if ($this->p[$k]) {
+          if(isset($d['method']) && $d['method']=='mining.submit' && isset($d['params']) && isset($d['params'][0]) and $d['params'][0]==$this->o[$k]->P['user'])
+            $this->o[$k]->t(-$d['id']);
+          $this->l('server '.$k.' gets '.$_d);
+          $this->p[$k]->write($_d);
+        } else $this->k($k, 'lost server');
+      } else $this->k($k, 'said garbage');
     }
   }
 
-  private function c($k, $o = 0) {
-    if ($this->p[$k]) socket_close($this->p[$k]);
-    $this->p[$k] = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-    if (!($p = $this->o[$k]->c($this->p[$k], $o))) $this->k($k, 'lost pools');
-    else if ($this->o[$k]->s) {
-      socket_write($this->p[$k], $this->o[$k]->s[1]);
-      socket_write($this->p[$k], '{"id": '.($this->o[$k]->s[0]+1).', "method": "mining.authorize", "params": ["'.$p['user'].'", "'.$p['pass'].'"]}'."\n");
-      $this->o[$k]->I = array();
-      $this->l($k.' connected to '.$p['url'].':'.$p['port'].' as '.$p['user'].'.');
-    } else $this->k($k, 'miss subscribe');
+  private function c($l, $k, $o = 0) {
+    if ($this->p[$k]) $this->p[$k]->end();
+    $this->p[$k] = NULL;
+    $a = $this->o[$k]->c();
+    $this->o[$k]->P = $a[$o];
+    $n = isset($a[$o+1]) ? $o+1 : 0;
+    $x = new React\Dns\Resolver\Factory();
+    $c = new React\SocketClient\Connector($l, $x->createCached('8.8.8.8', $l));
+    $c->create($this->o[$k]->P['url'], $this->o[$k]->P['port'])->then(function ($s) use ($k) {
+      $this->p[$k] = $s;
+      if (isset($this->s[$k]) && $this->o[$k]->s) {
+        $this->p[$k]->write($this->o[$k]->s[1]);
+        $this->p[$k]->write('{"id": '.($this->o[$k]->s[0]+1).', "method": "mining.authorize", "params": ["'.$this->o[$k]->P['user'].'", "'.$this->o[$k]->P['pass'].'"]}'."\n");
+        $this->o[$k]->I = array();
+        $this->l(((int)$k).' connected to '.$this->o[$k]->P['url'].':'.$this->o[$k]->P['port'].' as '.$this->o[$k]->P['user'].'.');
+        $s->on('close', function ($s) {
+          $this->k(array_search($s, $this->p), 'server gone');
+        });
+        $s->on('data', function ($__d, $s) {
+          $k = array_search($s, $this->p);
+          if (isset($this->s[$k])) {
+            foreach(array_filter(explode(PHP_EOL, $__d)) as $_d) {
+              if ($_d === FALSE || !($d = json_decode($_d, TRUE))) $this->k($k, 'server lost');
+              if (isset($d['id']) && $d['id'] && $d['id'] == $this->o[$k]->s[0]) {
+                if (isset($d['result']) && isset($d['result'][1]) && $d['result'][1]) {
+                  $this->l(((int)$k).' gets extranonce ["'.$d['result'][1].'", '.$d['result'][2].'].');
+                  $this->s[$k]->write('{"params":["'.$d['result'][1].'",'.$d['result'][2].'],"method":"mining.set_extranonce","id":null}'."\n");
+                }
+              } else if(!isset($d['method']) || $d['method']!='client.show_message') {
+                $this->l(((int)$k).' gets: '.$_d);
+                $this->s[$k]->write($_d."\n");
+              }
+              if (isset($d['method']) && $d['method']=='mining.set_difficulty' && isset($d['params']) && isset($d['params'][0]))
+                $this->o[$k]->F = $d['params'][0];
+              if (isset($d['result']) && $d['result']===TRUE && isset($d['id']) && $d['id'])
+                $this->o[$k]->t($d['id']);
+            }
+          } else $this->k($k, 'lost before server');
+        });
+      } else $this->k($k, 'miss subscribe');
+    }, function() use ($k, $n) {
+      if ($n) $this->c($l, $k, $n);
+      else $this->k($k, 'lost pools');
+    });
   }
 
   private function k($k, $m) {
@@ -110,10 +118,10 @@ class Stratum {
     $this->s = array_values($this->s);
     $this->p = array_values($this->p);
     $this->o = array_values($this->o);
-    $this->l($k.' '.$m.', killed.');
+    $this->l(((int)$k).' '.$m.', killed.');
   }
 
-  private function h($h) {
+  private function h($h, $c, $l) {
     $this->l('HTTP request '.$h);
     $d = array('result'=>NULL);
     if (($h = @json_decode($h, TRUE)) && isset($h['method']))
@@ -138,11 +146,11 @@ class Stratum {
         case 'switchpool':
           foreach($this->o as $k => $o) {
             if (!$o || is_null($o->u) || $o->u!=$h['params'][0]) continue;
-            $this->c($k, $h['params'][1]);
+            $this->c($l, $k, $h['params'][1]);
           }
           break;
       }
-    return json_encode($d);
+    $c->write(json_encode($d)."\n");
   }
 
   private function l($m) { # return;
@@ -168,14 +176,12 @@ class U {
     'pass'=> 'x'
   );
 
-  public function c($p, $o = 0) {
-    if (is_null($this->u)) return FALSE;
+  public function c() {
+    if (is_null($this->u)) return array();
     $m = new M();
-    foreach($m->q('SELECT pools.id, pools.url, pools.port, pools.user, pools.pass FROM pools JOIN workers ON workers.worker = "%s" WHERE pools.worker_id = workers.id;', $this->u) as $_p)
-      if ($o && $_p['id']!=$o) continue;
-      else if (@socket_connect($p, $_p['url'], $_p['port'])) return $this->P = $_p;
-    if (@socket_connect($p, $this->P['url'], $this->P['port'])) return $this->P;
-    return FALSE;
+    $q = $m->q('SELECT pools.id, pools.url, pools.port, pools.user, pools.pass FROM pools JOIN workers ON workers.worker = "%s" WHERE pools.worker_id = workers.id;', $this->u);
+    array_push($q, $this->P);
+    return $q;
   }
 
   public function t($I) {
