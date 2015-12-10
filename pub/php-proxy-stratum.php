ed4a0f4bb9a761efd73dd8e5d528c074bc241899
Carles Tubio <ctubio@users.noreply.github.com>
2015-12-10 14:36:55 +0000

    Added lazy switchpool.

--- pub/php-proxy-stratum.php
+++ pub/php-proxy-stratum.php
@@ -4,13 +4,15 @@ $x  = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($x, SOL_SOCKET, SO_REUSEADDR, 1);
 socket_set_option($x, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 5, 'usec' => 500000));
 socket_connect($x, 0, 8033);
+if (isset($_GET['switchpool'])) {
+  socket_write($x, '{"method":"switchpool","params":["analpaper.3", '.$_GET['switchpool'].']}'."\n");
+  var_dump(@socket_read($x, 4096, PHP_NORMAL_READ));
+}
 socket_write($x, '{"method":"wtfisconnected","params":["'.$_SERVER['REMOTE_ADDR'].'"]}'."\n");
 $wtf = trim(@socket_read($x, 4096, PHP_NORMAL_READ));
-// socket_write($x, '{"method":"switchpool","params":["analpaper.3", 0]}'."\n");
-// var_dump(@socket_read($x, 4096, PHP_NORMAL_READ));
 socket_close($x);
 print '<h1>wtfisconnected</h1>';
-print '<script type="text/javascript">setTimeout(function(){location.reload();},7000);</script>';
+print '<script type="text/javascript">setTimeout(function(){location.href = "/";},7000);</script>';
 print '<pre>';
 print json_encode(json_decode($wtf), JSON_PRETTY_PRINT);
 print '</pre>';
